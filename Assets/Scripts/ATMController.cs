d992b41db3d0176cb0a7bf5bb5a3ea238e6fe647
Kim-Hj0 <149381535+Kim-Hj0@users.noreply.github.com>
2023-12-13 16:14:19 +0900

    입금창에서 뒤로 가기 버튼
    입금창에서 뒤로 가기 버튼을 누르면
    이전 창으로 돌아간다.

--- Assets/Scripts/ATMController.cs
+++ Assets/Scripts/ATMController.cs
@@ -5,11 +5,12 @@ using UnityEngine.UI;
 
 public class ATMController : MonoBehaviour   
 {
-
     public Text balanceText;
     public GameObject depositPanel; //입금창
     public GameObject withdrawPanel;  //출금창
     public GameObject insufficientFundsPopup;   // 팝업 창 추가
+
+    private GameObject previousPanel; // 이전 패널 상태를 저장하기 위한 변수
 
     private float balance = 100000f;    // 가지고 있는 돈은 100,000원으로 초기화
 
@@ -66,6 +67,26 @@ public class ATMController : MonoBehaviour
             ShowInsufficientFundsPopup();
         }
     }
+
+    public void GoBack() //뒤로 가기
+    {
+        if(previousPanel != null)
+        {
+            //이전 패널 상태로 복원
+            previousPanel.SetActive(true);
+        }
+
+        //현재 패널이 depositPanel이라고 가정.
+        if (depositPanel != null)
+        {
+            depositPanel.SetActive(false);
+        }
+        
+        
+    }
+
+
+
 
     private void UpdateBalanceText()    //회색 부분의 현재 금액.
     {
