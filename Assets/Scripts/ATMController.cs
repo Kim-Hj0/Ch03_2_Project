0799bb2d7ae9b27ba04525703bd516c2c76c8079
Kim-Hj0 <149381535+Kim-Hj0@users.noreply.github.com>
2023-12-13 21:03:22 +0900

    입금 기능을 넣어보려고 했다.
    스크립트는 검색, gpt해서 적긴 했지만 이 이상은 모르겠다.

--- Assets/Scripts/ATMController.cs
+++ Assets/Scripts/ATMController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,7 +17,6 @@ public class ATMController : MonoBehaviour
     private GameObject previousPanel; // 이전 패널 상태를 저장하기 위한 변수
 
 
-
     private float balance = 100000f;    // 가지고 있는 돈은 100,000원으로 초기화
 
     private void Start()
@@ -31,8 +31,6 @@ public class ATMController : MonoBehaviour
         withdrawPanel.SetActive(false);
         insufficientFundsPopup.SetActive(false);//추가
 
-        mainMenuPanel.SetActive(true);//추가
-
 
         UpdateBalanceText();//추가
 
@@ -42,8 +40,6 @@ public class ATMController : MonoBehaviour
     {
         //입금창을 보이게 함
         depositPanel.SetActive(true);
-        previousPanel = mainMenuPanel;//추가
-        mainMenuPanel.SetActive(false);
     }
 
     public void ShowWithdrawPanel()
@@ -57,56 +53,19 @@ public class ATMController : MonoBehaviour
     {
         if(balance + amount <= 100000f)
         {
-            //balance += amount;
-            //UpdateBalanceText();
-            ////입금 후, 입금창을 숨김
-            //depositPanel.SetActive(false);
-
-            depositManager.DepositAmount(amount);
+            balance += amount;
+            UpdateBalanceText();
             depositPanel.SetActive(false);
             mainMenuPanel.SetActive(true);
-        //}
-        //else
-        //{
-        //    //가지고 있는 돈을 초과하는 경우에 대한 처리
-        //    ShowInsufficientFundsPopup();
+            
+        }
+        else
+        {
+           //가지고 있는 돈을 초과하는 경우에 대한 처리
+            ShowInsufficientFundsPopup();
         }
     }
 
-    public void Deposit10000()  //추가
-    {
-        DepositAmount(10000);
-    }
-
-    public void Deposit50000()//추가
-    {
-        DepositAmount(50000);
-    }
-
-    public void Deposit30000()//추가
-    {
-        DepositAmount(30000);
-    }
-
-    public void DepositCustomAmount()//추가.
-    {
-        string customAmountText = GetCustomAmountInput(); // 사용자가 직접 입력한 값을 가져오도록 수정해야 합니다.
-        depositManager.DepositCustomAmount(customAmountText);
-        depositPanel.SetActive(false);
-        mainMenuPanel.SetActive(true);//추가.
-    }
-
-    private void UpdateBalanceText()//추가
-    {
-        depositManager.UpdateBalanceText();//추가
-    }
-
-    private void ShowInsufficientFundsPopup()
-    {
-        insufficientFundsPopup.SetActive(true);
-    }
-
-    
 
     public void BackButton()
     {
@@ -115,17 +74,6 @@ public class ATMController : MonoBehaviour
         insufficientFundsPopup.SetActive(false);
         mainMenuPanel.SetActive(true);
     }
-
-    private string GetCustomAmountInput()
-    {
-        // 여기에 사용자가 직접 입력한 값을 가져오는 로직을 추가하세요.
-        // Unity UI의 InputField 등을 사용하여 값을 입력받을 수 있습니다.
-        // 현재는 임시로 "입력한 금액"으로 대체하였습니다.
-        return "입력한 금액";
-    }
-
-
-
 
     //public void WithdrawAmoun(float amount) //출금창.
     //{
@@ -143,7 +91,7 @@ public class ATMController : MonoBehaviour
     //    }
     //}
 
-    public void GoBack() //뒤로 가기
+    public void GoBack() //입금창에서 뒤로 가기
     {
         if(previousPanel != null)
         {
@@ -158,7 +106,7 @@ public class ATMController : MonoBehaviour
         }     
     }
 
-    public void GoBackWithdrawal() 
+    public void GoBackWithdrawal() //출금창에서 뒤로 가기
     {
         if(previousPanel != null)
         {
@@ -173,16 +121,17 @@ public class ATMController : MonoBehaviour
 
     }
 
-    //private void UpdateBalanceText()    //회색 부분의 현재 금액.
-    //{
-    //    balanceText.text = balance.ToString("   000,000");
-    //}
+    private void UpdateBalanceText()    //회색 부분의 현재 금액.
+    {
+        balanceText.text = balance.ToString("C");
+    }
 
-    //private void ShowInsufficientFundsPopup()
-    //{
-    //    // 잔액이 부족할 때 팝업 창을 보이게 함
-    //    insufficientFundsPopup.SetActive(true);
-    //}
+    private void ShowInsufficientFundsPopup()
+    {
+        // 잔액이 부족할 때 팝업 창을 보이게 함
+        insufficientFundsPopup.SetActive(true);
+    }
+
 
     public void CloseInsufficientFundsPopup()
     {
--- Assets/Scripts/DepositManager.cs
+++ Assets/Scripts/DepositManager.cs
@@ -8,6 +8,7 @@ public class DepositManager : MonoBehaviour
 {
     private float balance;
     private Text balanceText;
+    private object customAmountInputField;
 
     public void Initialize(float initialBalance, Text balanceText)
     {
@@ -15,8 +16,6 @@ public class DepositManager : MonoBehaviour
         this.balanceText = balanceText;
         UpdateBalanceText();
     }
-
-
 
     public void DepositAmount(float amount)
     {
@@ -29,6 +28,51 @@ public class DepositManager : MonoBehaviour
         {
             // 처리 로직 추가 (예: 잔액 부족 팝업 표시)
         }
+    }
+
+    public void Deposit10000()  //추가
+    {
+        DepositAmount(10000);
+    }
+
+    public void Deposit50000()//추가
+    {
+        DepositAmount(50000);
+    }
+
+    public void Deposit30000()//추가
+    {
+        DepositAmount(30000);
+    }
+
+    //사용자 정의 입금 금액에 대한
+    public void DepositCustomAmount()
+    {
+        string customAmountText = GetCustomAmountInput();
+        if (float.TryParse(customAmountText, out float customAmount))
+        {
+            DepositAmount(customAmount);
+        }
+        else
+        {
+            //잘못된 입력(숫자가 아닌 경우)
+            Debug.LogError("올바른 입력을 해주세요");
+        }
+    }
+
+    private string GetCustomAmountInput()
+    {
+        string customAmountText = customAmountInputField.Text;
+        if (float.TryParse(customAmountText, out float customAmount))
+        {
+            DepositAmount(customAmount);
+        }
+        else
+        {
+            Debug.LogError("Invalid custom amount input!");
+
+        }
+            return "입력한 금액";
     }
 
     public void DepositCustomAmount(string customAmountText)
@@ -45,6 +89,7 @@ public class DepositManager : MonoBehaviour
         if (balanceText != null)
             balanceText.text = balance.ToString("   000,000");
     }
+
 
     internal void CloseInsufficientFundsPopup()
     {
